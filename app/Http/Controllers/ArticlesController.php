5c35286fd652a9f6a07e3e976a5e8cea4d877047
acerjak <62491401+acerjak@users.noreply.github.com>
2020-10-01 12:22:13 -0700

    notes added to highlight the 7 restful controller actions to Articles controller

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -7,19 +7,47 @@ use App\Models\Article;
 
 class ArticlesController extends Controller
 {
-    //create funtion to show articles
+    
     public function show($id)
     {
+        //Render a list of a resource
         $article = Article::find($id);
 
         return view('articles.show', ['article' => $article]);
     }
 
-    //create function for index of articles
     public function index()
     {
+        //Show a single resource
         $articles = Article::latest()->get();
 
         return view('articles.index', ['articles' => $articles]);
+    }
+
+    public function create()
+    {
+        //Shows a view to create a new resource
+        
+    }
+
+    public function store()
+    {
+        //Persist the new resouce 
+
+    }
+
+    public function edit()
+    {
+        //Show a view to edit an existing resource
+    }
+
+    public function update()
+    {
+        //Persist the edited resource
+    }
+
+    public function destroy()
+    {
+        //Delete the resouce
     }
 }
