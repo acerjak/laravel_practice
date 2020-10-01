31b32e76590871b7b21c9ab6594c2519468fef21
acerjak <62491401+acerjak@users.noreply.github.com>
2020-10-01 13:46:10 -0700

    updated with create form and changed to reflect updating the article; also populates with current article parameters in input fields

---
+++ resources/views/articles/edit.blade.php
@@ -0,0 +1,45 @@
+@extends('layout')
+
+@section('content')
+    <div>
+        <div class="container">
+            <h1>UPDATE ARTICLE</h1>
+
+            <form method="POST" action="/articles">
+            <!-- prevents malicious users on other servers from faking form requests on your server -->
+            @csrf
+                <div class="field">
+                    <label class="label" for="title">Title</label>
+
+                    <div class="control">
+                        <input class="input" type="text" name="title" id="title" value="{{ $article->title }}">
+                    </div>
+                </div>
+
+                <div class="field">
+                    <label class="label" for="excerpt">Excerpt</label>
+
+                    <div class="control">
+                        <textarea class="textarea" name="excerpt" id="excerpt">{{ $article->excerpt }}</textarea>
+                    </div>
+                </div>
+
+                <div class="field">
+                    <label class="label" for="body">Body</label>
+                    
+                    <div class="control">
+                        <textarea class="textarea" name="body" id="body">{{ $article->body }}</textarea>
+                    </div>
+                </div>
+
+                <div class="field">
+                    <div class="control">
+                        <button class="button" type="submit">Submit</button>
+                    </div>
+                </div>
+
+            </form>
+        </div>
+    </div>
+
+@endsection
