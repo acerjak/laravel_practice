5f15c6473cf37594ed03ca43505df40984c1480e
acerjak <62491401+acerjak@users.noreply.github.com>
2020-10-01 11:53:27 -0700

    now populates the show blade for each article

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Article;
 
 class ArticlesController extends Controller
 {
     //create funtion to show articles
-    public function show($articleId)
+    public function show($id)
     {
-        dd($articleId);
+        $article = Article::find($id);
+
+        return view('articles.show', ['article' => $article]);
     }
-};
+}
