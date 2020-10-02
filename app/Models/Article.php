2decffc52fa1a19c95d334af727cb84966861cc1
acerjak <62491401+acerjak@users.noreply.github.com>
2020-10-02 11:34:25 -0700

    avoiding mass assignment issues by defining what is fillable or guarded

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -2,10 +2,16 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+// use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model
 {
-    use HasFactory;
+    //in order to avoid mass assignment issues, need to define what is 'fillable'
+    protected $fillable = ['title', 'excerpt', 'body'];
+
+    //if you want to be in charge of what is passed to the database and to not 'guard' anything
+    //protected $guarded = []
+
+    // use HasFactory;
 }
