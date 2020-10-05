a5571c02708233c245c09dff1bd9e854ed9d5c94
acerjak <62491401+acerjak@users.noreply.github.com>
2020-10-05 16:53:12 -0700

    relationship added for a User, also added necessary lines for the factory function to work

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -2,11 +2,14 @@
 
 namespace App\Models;
 
-// use Illuminate\Database\Eloquent\Factories\HasFactory;
+//make sure to use HasFactory to define the factory()
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model
 {
+    //make sure to add this below or the method will be undefined
+    use HasFactory;
     //in order to avoid mass assignment issues, need to define what is 'fillable'
     protected $fillable = ['title', 'excerpt', 'body'];
 
@@ -17,4 +20,11 @@ class Article extends Model
         return route('articles.show', $this);
     }
     // use HasFactory;
+
+    //need a user relationship
+    public function user()
+    {
+        //select * from user where article_id = 1
+        return $this->belongsTo(User::class);
+    }
 }
