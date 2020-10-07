b64e7fb48981a34574a9a6c7df2f91a9339e41e1
acerjak <62491401+acerjak@users.noreply.github.com>
2020-10-06 17:26:10 -0700

    added migration for a tags table

---
+++ database/migrations/2020_10_06_233345_create_tags_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTagsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tags', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->timestamps();
+        });
+
+        //linking table
+        Schema::create('article_tag', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('article_id');
+            $table->unsignedBigInteger('tag_id');
+            $table->timestamps();
+
+            //the combo of the article and the tag id must be unique so there are no duplicates
+            $table->unique(['article_id', 'tag_id']);
+
+            //add foreign keys
+            //the article id references the id column on the articles table and if we delete the article, lets cascade and delete this article as well
+            //for articles table
+            $table->foreign('article_id')->references('id')->on('articles')->onDelete('cascade');
+            //for tags table
+            $table->foreign('tag_id')->references('id')->on('tags')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tags');
+    }
+}
