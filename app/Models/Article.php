601272d4066ec54620e2c493e4e35eca8d068ae1
acerjak <62491401+acerjak@users.noreply.github.com>
2020-10-06 17:26:51 -0700

    added relationship between tags and articles within the articles model

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -21,10 +21,23 @@ class Article extends Model
     }
     // use HasFactory;
 
-    //need a user relationship
-    public function user()
+    //need an author/user relationship
+    public function author()
     {
         //select * from user where article_id = 1
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class, 'user_id');
+    }
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class);
+
+        //an article has many tags ex: label an article with (work, php, laravel)
+        //does a tag belong to an article? Not quite
+        //ex:
+        //article: Learn Laravel
+        //tags: php, laravel, work, education
+        //does laravel tag belong to this article? NO, tags belong to many articles
+        //an article can have many tags but a tag can have many articles
+        //a many to many relationship
     }
 }
