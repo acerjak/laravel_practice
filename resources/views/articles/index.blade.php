a37e7d9ec05a10646ed2949eb38c32ad3231ad11
acerjak <62491401+acerjak@users.noreply.github.com>
2020-10-07 19:17:38 -0700

    added styling to the articles index

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -1,16 +1,18 @@
 @extends ('layout')
 
 @section ('content')
-<h2>Articles</h2>
-<ul>
-    @forelse ($articles as $article)
-    <li>
-        <a href="{{ $article->path() }}"><h3>{{ $article->title }}</h3></a>
-        <p>{{ $article->excerpt }}</p>
-    </li>
-    @empty
-        <p>No relevant articles yet!</p>
-    @endforelse
-</ul>
+
+<h2 class="is-child notification is-info title has-text-centered mx-6">Index</h2>
+<div class="tile is-parent is-vertical">
+        
+            @forelse ($articles as $article)
+                <article class="tile is-child notification is-primary">
+                    <a class="title" href="{{ $article->path() }}"><h3>{{ $article->title }}</h3></a>
+                    <p>{{ $article->excerpt }}</p>
+                </article>    
+            @empty
+                <p>No relevant articles yet!</p>
+            @endforelse
+</div>
 
 @endsection
